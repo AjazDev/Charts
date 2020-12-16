e13955aebc8fc51b737bc36a3b0bb5593a98b81f
Ajaz Ahmed <72081378+AjazDev@users.noreply.github.com>
2020-12-16 13:13:00 +0500

    Update Charts.h

--- Source/Supporting Files/Charts.h
+++ Source/Supporting Files/Charts.h
@@ -10,12 +10,12 @@
 //
 
 #include <TargetConditionals.h>
-
-#if TARGET_OS_IPHONE || TARGET_OS_TV || TARGET_IPHONE_SIMULATOR
-	#import <UIKit/UIKit.h>
-#else
-    #import <Cocoa/Cocoa.h>
-#endif
+#import <UIKit/UIKit.h>
+// #if TARGET_OS_IPHONE || TARGET_OS_TV || TARGET_IPHONE_SIMULATOR
+// 	#import <UIKit/UIKit.h>
+// #else
+//     #import <Cocoa/Cocoa.h>
+// #endif
 
 //! Project version number for Charts.
 FOUNDATION_EXPORT double ChartsVersionNumber;
